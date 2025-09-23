790ebde173902dec10b476f5c52bc56a24a2e305
leongpet <leongj.peter@gmail.com>
2025-09-23 11:02:15 -0400

    started lab3

---
+++ Labs/Lab_3/Part_1/part1.sv
@@ -0,0 +1,9 @@
+module part1(
+    input logic [3:0] a, b,
+    input logic c_in,
+    output logic [3:0] s, c_out
+);
+
+
+
+endmodule
---
+++ Labs/Lab_3/Part_2/part2.sv
@@ -0,0 +1,8 @@
+module part2(
+    input logic [3:0] A, B, 
+    input logic [1:0] Function,
+    output logic [7:0] ALUout
+);
+
+
+endmodule
